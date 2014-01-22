74fa0c0bc05ffcecc2dc0e3a4f5ba06c10e9f7fd
zc238 <zc238@cornell.edu>
2014-01-22 06:29:08 -0800

    stupid error

--- src/sep/pack/data/TICKER.java
+++ src/sep/pack/data/TICKER.java
@@ -7,7 +7,7 @@ public class TICKER {
 	public static final String SPY = "SPY";
 	public static final String SPX = "SPXU";
 	public static final String UPR = "UPRO";
-	public static final String SDS = "SPY";
+	public static final String SDS = "SDS";
 	public static final String SH = "SH";
 	public static final String SSO = "SSO";
 	
